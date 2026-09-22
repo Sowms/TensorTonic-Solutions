ff97b2872659c086b142174e2c5c2da6102e29ea
Sowmya S Sundaram <sowmyassundaram@gmail.com>
2026-09-22 23:16:49 +0530

    Solve logistic-regression-training

---
+++ logistic-regression-training/logistic-regression-training.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def _sigmoid(z: np.ndarray) -> np.ndarray:
+    """
+    Returns elementwise sigmoid values.
+    """
+    return np.where(z >= 0, 1/(1+np.exp(-z)), np.exp(z)/(1+np.exp(z)))
+
+def train_logistic_regression(X: np.ndarray, y: np.ndarray, lr: float = 0.1, steps: int = 1000) -> tuple[np.ndarray, float]:
+    """
+    Returns the trained weights and bias as (w, b).
+    """
+    w = np.zeros(X.shape[1])
+    b = 0
+    for i in range(0, steps):
+        z = np.dot(X, w) + b
+        h = _sigmoid(z)
+
+        dw = X.T @ (h-y) / len(y)
+        db = np.mean(h-y)
+
+        w = w - lr*dw
+        b = b - lr*db
+
+    return w, b
